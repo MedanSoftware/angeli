e63b22fd6dd269d117861c12f071e8f202f65135
Agung Dirgantara <agungmasda29@gmail.com>
2020-05-10 14:49:03 +0700

    Extend \Illuminate\Database\Eloquent\Model class

---
+++ application/core/Eloquent_Model.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package Codeigniter
+ * @subpackage Eloquent Model 
+ * @category Library
+ * @author Agung Dirgantara <agungmasda29@gmail.com>
+ */
+
+namespace Angeli\Model;
+
+abstract class Eloquent_Model extends \Illuminate\Database\Eloquent\Model
+{
+	
+}
+
+/* End of file Eloquent_Model.php */
+/* Location : ./application/core/Eloquent_Model.php */
