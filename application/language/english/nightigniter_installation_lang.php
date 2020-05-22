793cd71276cafa87efa08a4e71c5d4bbf0e2b390
Agung Dirgantara <agungmasda29@gmail.com>
2020-05-22 11:13:37 +0700

    Add codeigniter system languages @0f55214

---
+++ application/language/english/nightigniter_installation_lang.php
@@ -0,0 +1,27 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * @package Codeigniter
+ * @subpackage Nightigniter Installation
+ * @category Language
+ * @author Agung Dirgantara <agungmasda29@gmail.com>
+ */
+
+$lang['welcome_to_installation'] = 'Welcome to installation!';
+$lang['please_add_root_password'] = 'Please add root password';
+$lang['web_installer'] = 'Web Installer';
+$lang['sidebar_left_menu_home'] = 'Home';
+$lang['sidebar_left_menu_database'] = 'Database';
+$lang['sidebar_left_menu_application'] = 'Application';
+$lang['sidebar_left_menu_language'] = 'Language';
+$lang['sidebar_left_menu_password'] = 'Password';
+$lang['sidebar_left_menu_environment'] = 'Environment';
+$lang['sidebar_left_menu_timezone&map'] = 'Timezone & Map';
+$lang['sidebar_left_menu_guide'] = 'Guide';
+$lang['page_rendered_in'] = 'Page rendered in';
+$lang['copyright'] = 'Copyright';
+$lang['version'] = 'Version';
+
+$lang['old_password'] = 'Old password';
+
+/* End of file nightigniter_installation_lang.php */
+/* Location : ./application/language/english/nightigniter_installation_lang.php */
